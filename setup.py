1644788550f340ddcf20ef9e9f130001334800ec
Yuheng Huo <39151480+Neowless@users.noreply.github.com>
2024-05-28 11:55:43 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name = 'ScopeFoundryHW.random_gen',
+    
+    version = '0.0.1',
+    
+    description = 'ScopeFoundry Hardware plug-in: Dummy random number generator',
+    
+    # Author details
+    author='Edward S. Barnard',
+    author_email='esbarnard@lbl.gov',
+
+    # Choose your license
+    license='BSD',
+
+    package_dir={'ScopeFoundryHW.random_gen': '.'},
+    
+    packages=['ScopeFoundryHW.random_gen',],
+    
+    #packages=find_packages('.', exclude=['contrib', 'docs', 'tests']),
+    #include_package_data=True,  
+    
+    package_data={
+        '':["README*", 'LICENSE', # include License and readme 
+            "*.ui", # include QT ui files 
+            ], 
+    }
+    )
